c0edd7978948eae81c1e83878ea63df22301add9
Olehpryimak <31596938+Olehpryimak@users.noreply.github.com>
2018-03-25 18:09:15 +0300

    conference - practice5 (2 part)

--- src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -87,10 +87,14 @@ public class ConferenceApi {
         	displayName = extractDefaultDisplayNameFromEmail(mainEmail);
         // Create a new Profile entity from the
         // userId, displayName, mainEmail and teeShirtSize
-        Profile profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
-
+        Profile profile = getProfile(user);
+        if(profile == null)
+           profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
+        else
+        	profile.update(displayName,teeShirtSize);
         // TODO 3 (In Lesson 3)
         // Save the Profile entity in the datastore
+        ofy().save().entity(profile).now();
 
         // Return the profile
         return profile;
@@ -114,9 +118,10 @@ public class ConferenceApi {
 
         // TODO
         // load the Profile Entity
-        String userId = ""; // TODO
-        Key key = null; // TODO
-        Profile profile = null; // TODO load the Profile entity
+        String userId = user.getUserId(); // TODO
+        Key key = Key.create(Profile.class,userId); // TODO
+        
+        Profile profile = (Profile) ofy().load().key(key).now(); // TODO load the Profile entity
         return profile;
     }
 }
